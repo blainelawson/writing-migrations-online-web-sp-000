2c8743f7f19d232aee222bbc7228250d5223704e
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-29 14:56:32 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-class Add_grade_and_birthdate_columns < ActiveRecord::Migration[5.1]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
     add_column :grade, :integer
     add_column :birthdate, :string
